2ebbbc4b58ecd4d3b5fdaa21ae784fe99d414642
jeevanaMenta <jeevana.menta@transferwise.com>
2023-03-26 17:35:46 +0100

    Completing the tranfer flow

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -23,20 +23,37 @@ public class DemoApplication {
 
 		final CreateQuoteRequest request = getCreateQuoteRequest(scanner);
 		final var service = new transferService();
-		final var response = service.createQuote(request);
+		final var tokenResponse = service.generateToken();
+
+		// Sample code to parse the response and get any field
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(tokenResponse);
+		JsonNode tokenNode =  mapper.readTree(json).get("access_token");
+		final var token = tokenNode.textValue();
+
+		final var response = service.createQuote(request, token);
 
 		if(response == null) {
 			System.out.println("Quote creation failed");
 		}
 
-		// Sample code to parse the response and get any field
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(response);
-		JsonNode jsonNode = mapper.readTree(json);
+		String stringJson = mapper.writeValueAsString(response);
+		JsonNode jsonNode = mapper.readTree(stringJson);
 		JsonNode idNode = jsonNode.get("id");
-		System.out.printf("Created quote id %s%n", idNode.textValue());
+		final var quoteId = idNode.textValue();
+		System.out.printf("Created quote id %s%n", quoteId);
 
-		createRecipient(scanner, request.getTargetCurrency(), service);
+		final var recipientResponse = createRecipient(scanner, request.getTargetCurrency(), service, token);
+		JsonNode node = mapper.readTree(mapper.writeValueAsString(recipientResponse));
+		final var recipientId = node.get("id").intValue();
+		System.out.printf("Created recipient id %s%n", recipientId);
+
+		service.updateTransfer(quoteId, recipientId, token);
+		final var transferResponse = service.createTransfer(quoteId, recipientId, token);
+
+		JsonNode transferNode = mapper.readTree(mapper.writeValueAsString(transferResponse));
+		final var transferId = transferNode.get("id").intValue();
+		System.out.printf("Transfer created successfully and transfer id is %s%n", transferId);
 	}
 
 	private static CreateQuoteRequest getCreateQuoteRequest(Scanner scanner) {
@@ -44,6 +61,7 @@ public class DemoApplication {
 		String sourceCurrency = scanner.next();
 
 		System.out.println("Setting target currency to USD");
+		System.out.printf("Creating a transfer %S -> USD%n", sourceCurrency);
 		String targetCurrency = "USD";
 
 		System.out.println("How much would you like to transfer? Please enter the amount in numbers (up to 50,000).");
@@ -57,23 +75,23 @@ public class DemoApplication {
 		return request;
 	}
 
-	private static void createRecipient(Scanner scanner, String currency, transferService service) {
+	private static Object createRecipient(Scanner scanner, String currency, transferService service, String token) {
 		System.out.println("Please enter recipient account details:");
-		System.out.println("Account number");
+		System.out.println("Enter account number:");
 		String accountNumber = scanner.next();
 
-		System.out.println("Email");
+		System.out.println("Enter email:");
 		String email = scanner.next();
 
 		System.out.println("Account type:(eg CHECKING)");
 		String type = scanner.next();
 
 		// account details are specific to USD
-		System.out.println("Creating sender account...");
+		System.out.println("Creating receiver account...");
 		final var request = new CreateRecipientRequest();
 		request.setCurrency(currency);
 		request.setType("ABA");
-		request.setAccountHolderName("Sender account" );
+		request.setAccountHolderName("receiver account" );
 		final var details = new RecipientAccountDetails();
 		details.setAccountNumber(accountNumber);
 		details.setEmail(email);
@@ -81,8 +99,6 @@ public class DemoApplication {
 		details.setAddress(new Address());
 		request.setDetails(details);
 
-		final var createSenderResponse = service.createRecipient(request);
-
-		System.out.println("SUCCESSFUL!");
+		return service.createRecipient(request, token);
 	}
 }
--- src/main/java/com/example/demo/service/transferService.java
+++ src/main/java/com/example/demo/service/transferService.java
@@ -2,41 +2,75 @@ package com.example.demo.service;
 
 import com.example.demo.domain.CreateQuoteRequest;
 import com.example.demo.domain.CreateRecipientRequest;
+import com.example.demo.domain.CreateTransferRequest;
+import com.example.demo.domain.UpdateQuoteRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 public class transferService {
-    public Object generateToken(String basicAuth, String refreshToken) {
+    public Object generateToken() {
         return WebClient.builder().build().post()
                 .uri("https://api.sandbox.transferwise.tech/oauth/token")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, String.format("Basic %s", basicAuth)) // include basic auth token here
+                .header(HttpHeaders.AUTHORIZATION, "Basic dHctdGVzdC1iYW5rOnR3LXRlc3QtYmFuaw==")
                 .body(BodyInserters.fromFormData("grant_type", "refresh_token")
-                        .with("refresh_token", refreshToken)) // include refresh token here
+                        .with("refresh_token", "efd76f1c-f819-4138-b5f1-2fe69512cec7"))
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(Object.class).block();
     }
-    public Object createQuote(CreateQuoteRequest request) {
+    public Object createQuote(CreateQuoteRequest request, String token) {
         return WebClient.builder().build().post()
                 .uri("https://api.sandbox.transferwise.tech/v2/quotes")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer 1867b636-16c4-4d4f-adf5-d762a580c2f3")
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", token))
                 .body(Mono.just(request), CreateQuoteRequest.class)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(Object.class).block();
     }
 
-    public Object createRecipient(CreateRecipientRequest request) {
+    public Object createRecipient(CreateRecipientRequest request, String token) {
         return WebClient.builder().build().post()
                 .uri("https://api.sandbox.transferwise.tech/v1/accounts")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer <token>")
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", token))
                 .body(Mono.just(request), CreateRecipientRequest.class)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(Object.class).block();
+    }
+
+    public Object updateTransfer(String quoteId, int recipientId, String token) {
+        final UpdateQuoteRequest request = new UpdateQuoteRequest();
+        request.setTargetAccount(recipientId);
+
+        return WebClient.builder().build().patch()
+                .uri("https://api.sandbox.transferwise.tech/v2/quotes/".concat(quoteId))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", token))
+                .body(Mono.just(request), UpdateQuoteRequest.class)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(Object.class).block();
+    }
+
+    public Object createTransfer(String quoteId, int recipientId, String token) {
+        final CreateTransferRequest request = new CreateTransferRequest();
+        request.setTargetAccount(recipientId);
+        request.setQuoteUuid(quoteId);
+        request.setCustomerTransactionId(String.valueOf(UUID.randomUUID()));
+
+        return WebClient.builder().build().post()
+                .uri("https://api.sandbox.transferwise.tech/v1/transfers")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", token))
+                .body(Mono.just(request), CreateTransferRequest.class)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(Object.class).block();
